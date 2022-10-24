903990b605a133df88f120e80059050868e751bf
blazingly-fast <tehnoslavija@gmail.com>
2022-10-24 23:50:59 +0200

    signup and  token helpers

--- controllers/userController.go
+++ controllers/userController.go
@@ -25,20 +25,32 @@ func Login() gin.HandlerFunc {
 
 func Signup() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		// var c, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		var c, cancel = context.WithTimeout(context.Background(), 100*time.Second)
 		var user models.User
+		defer cancel()
 
 		if err := ctx.BindJSON(&user); err != nil {
 			ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 			return
 		}
-
 		validationErr := validate.Struct(user)
 		if validationErr != nil {
 			ctx.JSON(http.StatusBadRequest, gin.H{"error": validationErr.Error()})
 			return
 		}
 
+		var count int64
+		db.Model(&user).Where("email = ?", user.Email).Count(&count)
+		defer cancel()
+
+		if count > 0 {
+			ctx.JSON(http.StatusInternalServerError, gin.H{"error": "this email already exists"})
+		}
+
+		token, refreshToken, _ := helpers.GenerateAllTokens(*user.Email, *user.First_name, *user.Last_name, *user.User_type, *&user.User_id)
+		user.Token = &token
+		user.Refresh_token = &refreshToken
+		db.WithContext(c).Create(&user)
 	}
 }
 
@@ -50,7 +62,6 @@ func GetUsers() gin.HandlerFunc {
 
 func GetUser() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-
 		userId := ctx.Param("user_id")
 		if err := helpers.MatchUserTypeToUid(ctx, userId); err != nil {
 			ctx.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
--- helpers/tokenHelper.go
+++ helpers/tokenHelper.go
@@ -1,1 +1,52 @@
 package helpers
+
+import (
+	"log"
+	"os"
+	"time"
+
+	"github.com/blazingly-fast/social-network-api/database"
+	"github.com/golang-jwt/jwt"
+)
+
+var db = database.Init(os.Getenv("DATABASE_URL"))
+
+var SECRET_KEY string = os.Getenv("SECRET_KEY")
+
+type SignedDetails struct {
+	Email      string
+	First_name string
+	Last_name  string
+	Uid        string
+	User_type  string
+	jwt.StandardClaims
+}
+
+func GenerateAllTokens(email string, firstName string, lastName string, userType string, uid string) (token string, refreshToken string, err error) {
+	claims := &SignedDetails{
+		Email:      email,
+		First_name: firstName,
+		Last_name:  lastName,
+		Uid:        uid,
+		User_type:  userType,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Local().Add(time.Hour * time.Duration(24)).Unix(),
+		},
+	}
+
+	refreshClaims := &SignedDetails{
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Local().Add(time.Hour * time.Duration(168)).Unix(),
+		},
+	}
+	token, err = jwt.NewWithClaims(jwt.SigningMethodES256, claims).SignedString([]byte(SECRET_KEY))
+	refreshToken, err = jwt.NewWithClaims(jwt.SigningMethodES256, refreshClaims).SignedString([]byte(SECRET_KEY))
+
+	if err != nil {
+		log.Panic(err)
+		return
+	}
+
+	return token, refreshToken, err
+
+}
